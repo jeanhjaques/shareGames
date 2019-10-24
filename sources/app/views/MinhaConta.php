706f20aedb12e3702cb4089c4599c8b074e37007
Jean Jaques <45767643+jeanhjaques@users.noreply.github.com>
2019-10-23 22:25:18 -0400

    Update MinhaConta.php
    atualização de referencias de diretorios

--- sources/app/views/MinhaConta.php
+++ sources/app/views/MinhaConta.php
@@ -7,8 +7,8 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
-   		<link href="/sources/app/views/homepage.css" rel="stylesheet">
-    	<script type="text/javascript" src="../scripts/homepage.js"></script>
+   		<link href="EstiloPadrao.css" rel="stylesheet">
+    	<script type="text/javascript" src="homepage.js"></script>
 		<link href="https://fonts.googleapis.com/css?family=Bree+Serif&amp;display=swap" rel="stylesheet">
 		<title>Minha Conta</title>
 	</head>
