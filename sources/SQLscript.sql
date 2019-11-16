0f6bd40f02063704ccccd75af69397162242a7ef
Jean Jaques <45767643+jeanhjaques@users.noreply.github.com>
2019-11-15 23:18:12 -0400

    Add files via upload
    Add SQLscript

---
+++ sources/SQLscript.sql
@@ -0,0 +1,35 @@
+CREATE TABLE usuario(
+	nome VARCHAR(60) NOT NULL,
+	sobrenome VARCHAR(30) NOT NULL,
+    cpf VARCHAR(14) UNIQUE NOT NULL,
+    dataNascimento VARCHAR(10) NOT NULL,
+    email VARCHAR(40) NOT NULL,
+    senha VARCHAR(16) NOT NULL,
+    cep VARCHAR(6),
+    pais VARCHAR(40),
+    estado VARCHAR(40),
+    cidade VARCHAR(40),
+    bairro VARCHAR(60),
+    rua VARCHAR(60),
+    numero VARCHAR(10),
+    complemento VARCHAR(90),
+    idUsuario INTEGER AUTO_INCREMENT PRIMARY KEY
+);
+
+CREATE TABLE jogo(
+	nome VARCHAR(40) NOT NULL,
+    capa VARCHAR(100) NOT NULL,
+    anosUso INTEGER,
+    categoria VARCHAR(20) NOT NULL,
+    idUsuario INTEGER NOT NULL,
+    FOREIGN KEY(idUsuario) REFERENCES usuario(idUsuario)
+     ON DELETE cascade ON UPDATE cascade,
+    idJogo INTEGER AUTO_INCREMENT PRIMARY KEY
+);
+
+CREATE TABLE deseja(
+	idJogo INTEGER NOT NULL,
+    idUsuario INTEGER NOT NULL,
+    FOREIGN KEY(idJogo) REFERENCES jogo(idJogo),
+    FOREIGN KEY(idUsuario) REFERENCES usuario(idUsuario)
+);
