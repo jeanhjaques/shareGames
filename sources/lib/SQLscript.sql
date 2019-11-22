c543dddbb4de7e6a9dbe321a643276239fe9b7da
Jean Jaques <45767643+jeanhjaques@users.noreply.github.com>
2019-11-22 02:15:47 -0400

    Update SQLscript.sql

--- sources/lib/SQLscript.sql
+++ sources/lib/SQLscript.sql
@@ -41,6 +41,8 @@ CREATE TABLE troca(
     idJogoB INTEGER NOT NULL,
     idUsuarioB INTEGER NOT NULL,
     idTroca INTEGER AUTO_INCREMENT PRIMARY KEY,
+    estado INTEGER NOT NULL,
+    dataTroca TIMESTAMP NOT NULL,
     FOREIGN KEY(idJogoA) REFERENCES jogo(idJogo),
     FOREIGN KEY(idJogoB) REFERENCES jogo(idJogo),
     FOREIGN KEY(idUsuarioA) REFERENCES usuario(idUsuario),
