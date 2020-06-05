426d0d8bb34ab4bdb4f7ecbb756d41145b95cd03
Jean Jaques <45767643+jeanhjaques@users.noreply.github.com>
2020-06-04 21:56:00 -0400

    Delete index.php

--- sources/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require 'app/controllers/PagesController.php';
-
-	$controllerName = $_GET['controller'].'Controller';
-	$actionName = $_GET['action'];
-
-	$controller = new $controllerName();
-	$controller->$actionName(); 
-?>
